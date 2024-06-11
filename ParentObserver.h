52d6a2c025695b4d176d95c0b275d63a6c9fa5e2
Ron Zatkovsky <130716165+Chamomiletea2000@users.noreply.github.com>
2024-06-11 16:30:18 -0400

    Add files via upload

---
+++ ParentObserver.h
@@ -0,0 +1,37 @@
+﻿#ifndef ParentObserver_H
+#define ParentObserver_H
+
+class ParentObserver
+{
+	private:
+	std::vector<*Observer> *observers;
+	
+	public:
+	void Notify() {for (auto i : observers){i.Notify();}
+	};
+	void AddObserver(Observer *newobserver){};
+	void RemoveObserver(Observer *observer){};
+	
+}
+;
+	
+class Observer
+{
+	private:
+	
+	public:
+	virtual void Notify()=0;
+	virtual void Update()=0;
+}
+;
+
+class KeyObserver : public Observer
+{
+	private:
+	
+	public:
+	virtual void Notify(){Update();}
+	virtual void Update();
+}
+;
+#endif
